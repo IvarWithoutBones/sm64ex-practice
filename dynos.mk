8c661434c6f7fa1efc21702d9492de52d9349cdf
Ivar Scholten <ivar.scholten@protonmail.com>
2022-02-17 19:53:37 +0100

    Apply Dynos 1.1

---
+++ dynos.mk
@@ -0,0 +1,33 @@
+# ----------------------
+# Dynamic Options System
+# ----------------------
+
+DYNOS_INPUT_DIR := ./dynos
+DYNOS_OUTPUT_DIR := $(BUILD_DIR)/dynos
+DYNOS_PACKS_DIR := $(BUILD_DIR)/dynos/packs
+DYNOS_INIT := \
+    mkdir -p $(DYNOS_INPUT_DIR); \
+    mkdir -p $(DYNOS_OUTPUT_DIR); \
+    mkdir -p $(DYNOS_PACKS_DIR); \
+    cp -f -r $(DYNOS_INPUT_DIR) $(BUILD_DIR) 2>/dev/null || true ;
+
+DYNOS_DO := $(shell $(call DYNOS_INIT))
+INCLUDE_CFLAGS += -DDYNOS
+
+UNAME := $(shell uname -s)
+ifeq ($(UNAME),Linux)
+    INCLUDE_CFLAGS += -Wno-error=format-security -D_GLIBCXX_USE_CXX11_ABI=1 # Required for compiling with gcc/glibc
+    LDFLAGS += -lstdc++ # Prevents linker errors. Should be "LDFLAGS" instead on non-nix
+endif
+
+$(BUILD_DIR)/data/dynos_opt_vanilla_c.o: $(BUILD_DIR)/include/text_strings.h
+
+# ----
+# Coop
+# ----
+
+SM64EX_COOP := $(or $(and $(wildcard src/pc/network/network.h),1),0)
+ifeq ($(SM64EX_COOP),1)
+INCLUDE_CFLAGS += -DDYNOS_COOP
+endif
+
