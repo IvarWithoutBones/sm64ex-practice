c27a54985fc6021e76b9e1fd8817f317c645ac2f
Ivar Scholten <ivar.scholten@protonmail.com>
2022-02-17 19:58:35 +0100

    Add nix shell to use with direnv

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    python3
+    hexdump
+  ];
+
+  buildInputs = with pkgs; [
+    SDL2
+    audiofile
+  ];
+}
